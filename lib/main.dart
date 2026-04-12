7b1b303b6c506851dee02d6da11359995cd8ef39
HOME-PC\Илья <iluxx_ivanov@mail.ru>
2026-04-12 17:41:13 +0400

    hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
-import 'package:auth_front/registration_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:auth_front/login_page.dart';
 
-void main() {
-  runApp(const BasicApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  try {
+    await Firebase.initializeApp();
+    print('Firebase инициализирован успешно');
+  } catch (e) {
+    print('Ошибка инициализации Firebase: $e');
+  }
+  
+  runApp(const MyApp());
 }
 
-class BasicApp extends StatelessWidget {
-  const BasicApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Auth Front',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
       debugShowCheckedModeBanner: false,
-      home: RegistrationPage(),
     );
   }
 }
